2914c7fab6f141d73f6fe2fb1e2833b3918760f6
sberkekilic <sberkekilic@gmail.com>
2023-01-19 23:49:13 +0300

    Son hali

--- WinFormsApp1/Form3.Designer.cs
+++ WinFormsApp1/Form3.Designer.cs
@@ -35,12 +35,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(322, 113);
+            this.comboBox1.Location = new System.Drawing.Point(63, 61);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(167, 23);
             this.comboBox1.TabIndex = 0;
@@ -50,32 +57,35 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(322, 142);
+            this.comboBox2.Location = new System.Drawing.Point(63, 90);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(167, 23);
             this.comboBox2.TabIndex = 1;
             this.comboBox2.Text = "Boş";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(322, 171);
+            this.textBox1.Location = new System.Drawing.Point(63, 119);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(78, 23);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(406, 171);
+            this.button1.Location = new System.Drawing.Point(147, 119);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Sipariş ver";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(278, 116);
+            this.label1.Location = new System.Drawing.Point(19, 64);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 15);
             this.label1.TabIndex = 4;
@@ -84,7 +94,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(263, 145);
+            this.label2.Location = new System.Drawing.Point(4, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 15);
             this.label2.TabIndex = 5;
@@ -93,29 +103,81 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(278, 174);
+            this.label3.Location = new System.Drawing.Point(19, 122);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 15);
             this.label3.TabIndex = 6;
             this.label3.Text = "Miktar:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 155);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sipariş İsteme";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(262, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(526, 155);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sipariş Takip";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sorgula";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Sipariş Kodu:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form3";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -128,5 +190,10 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label4;
+        private TextBox textBox2;
+        private Button button2;
     }
 }
--- WinFormsApp1/Form3.cs
+++ WinFormsApp1/Form3.cs
@@ -9,6 +9,8 @@ using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WinFormsApp1
 {
@@ -19,21 +21,37 @@ namespace WinFormsApp1
             InitializeComponent();
         }
         SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
         private void Form3_Load(object sender, EventArgs e)
         {
-            SqlConnection cn = new SqlConnection("Data Source=DESKTOP-LIN8EHF;Initial Catalog=Drugs;Integrated Security=True;MultipleActiveResultSets=true");
+            cn = new SqlConnection("Data Source=DESKTOP-LIN8EHF;Initial Catalog=Drugs;Integrated Security=True;MultipleActiveResultSets=true");
             cn.Open();
+
             BindData();
+            //BindData2();
+
+            cn.Close();
+        }
+
+        public void BindData2()
+        {
+            string selected = comboBox1.GetItemText(comboBox1.SelectedItem);
+            cmd = new SqlCommand("select IlacKodu from Ilaclar where MarkaAdi=@x", cn);
+            cmd.Parameters.AddWithValue("@x", selected);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox2.Items.Add(dr[0].ToString());
+            }
+            dr.Close();
+            
         }
 
         public void BindData()
         {
-            SqlCommand cmd = new SqlCommand("select distinct MarkaAdi from Ilaclar", cn);
-            SqlDataReader dr = cmd.ExecuteReader();
-
-            SqlCommand cmd2 = new SqlCommand("select IlacKodu from Ilaclar where MarkaAdi=@x", cn);
-            SqlDataReader dr2 = cmd.ExecuteReader();
-
+            cmd = new SqlCommand("select distinct MarkaAdi from Ilaclar", cn);
+            dr = cmd.ExecuteReader();
             while (dr.Read())
             {
                 comboBox1.Items.Add(dr[0].ToString());
@@ -47,7 +65,63 @@ namespace WinFormsApp1
         }
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            comboBox2.Items.Clear();
+            cmd = new SqlCommand("select IlacKodu from Ilaclar where MarkaAdi=@x", cn);
+            cmd.Parameters.AddWithValue("@x", comboBox1.Text);
+            cn.Open();
+            dr= cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox2.Items.Add(dr[0].ToString());
+            }
+            cn.Close();
+            
         }
 
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> mylist = new List<string>(new string[] { "Yolda", "Hazırlanıyor", "Teslim edildi" });
+
+            Random R = new Random();
+
+            // get random number from 0 to 2. 
+            int someRandomNumber = R.Next(0, mylist.Count());
+            string ConnectionString = "Data Source=DESKTOP-LIN8EHF;Initial Catalog=Drugs;Integrated Security=True";
+
+            SqlConnection baglan = new SqlConnection(ConnectionString);
+
+            baglan.Open();
+
+            SqlCommand komut = new SqlCommand("insert into SiparisTakip (Marka, IlacKodu, SiparisAdet, SiparisTarihi, Durumu) values\r\n(@Marka, @IlacKodu, @SiparisAdet, @SiparisTarihi, @Durumu)", baglan);
+            komut.Parameters.AddWithValue("@Marka", (comboBox1.SelectedItem));
+            komut.Parameters.AddWithValue("@IlacKodu", (comboBox2.SelectedItem));
+            komut.Parameters.AddWithValue("@SiparisAdet", int.Parse(textBox1.Text));
+            komut.Parameters.AddWithValue("@SiparisTarihi", DateTime.Now.ToString("MM/dd/yyyy"));
+            komut.Parameters.AddWithValue("@Durumu", mylist.ElementAt(someRandomNumber));
+
+            SqlDataReader oku = komut.ExecuteReader();
+
+            baglan.Close();
+
+            var id = Guid.NewGuid().ToString();
+            string message = $"Sipariş Başarıyla Verildi\r\nSipariş Kodu:{id}";
+            string title = "Sipariş Verme";
+            MessageBox.Show(message, title);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
